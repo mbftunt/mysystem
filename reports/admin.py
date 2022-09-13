7effcace60e806255abcca19859b2d4017a977b5
tunt19 <mbftunt@gmail.com>
2022-09-13 08:10:58 +0700

    fix admin.py

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MyUser
+
 
 # Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'is_staff'
+    )
+
+
+admin.site.register(MyUser, MyUserAdmin)
